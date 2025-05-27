1448fb64b0c8cc75a0dcca60986ed87a54221c20
NguyenPhucPhi-it <phinp.24it@vku.udn.vn>
2025-05-27 14:14:39 +0700

    socket

---
+++ Socket_Multi_Thread_Client/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Socket_Multi_Thread_Client {
+	requires java.desktop;
+	requires java.sql;
+	requires flatlaf.demo;
+}
---
+++ Socket_Multi_Thread_Server/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Socket_Multi_Thread_Server {
+}
